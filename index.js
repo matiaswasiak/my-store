d310d0a409e65db9bee9248eed2097d892b98b99
matiaswasiak <matiaswasiak@gmail.com>
2023-07-28 14:27:10 -0300

    Separación de responsabilidades con express.Router

--- index.js
+++ index.js
@@ -1,59 +1,10 @@
 const express = require('express');
-const { faker } = require('@faker-js/faker');
+const routerApi = require('./routes');
+
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
-app.get('/nueva-ruta', (req, res) => {
-  res.send('Nueva ruta');
-});
-
-app.get('/products', (req, res) => {
-  const products = [];
-  const { size } = req.query;
-  const limit = size || 10;
-  for (let i = 0; i < limit; i++) {
-    products.push({
-      name: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price(), 10),
-      image: faker.image.urlLoremFlickr(),
-    });
-  }
-  res.json(products);
-});
-
-app.get('/products/filter', (req, res) => {
-  res.send('Soy un filter');
-});
-
-app.get('/products/:id', (req, res) => {
-  const { id } = req.params;
-  res.json({
-    id,
-    name: 'Producto 1',
-    price: 1000,
-  });
-});
-
-app.get('/users', (req, res) => {
-  const { limit, offset } = req.query;
-  if (limit && offset) {
-    res.json({ limit, offset });
-  } else {
-    res.send('No hay parametros');
-  }
-});
-
-app.get('/categories/:categoryId/products/:productId', (req, res) => {
-  const { categoryId, productId } = req.params;
-  res.json({
-    categoryId,
-    productId,
-  });
-});
+routerApi(app);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
---
+++ routes/products.router.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const { faker } = require('@faker-js/faker');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  const products = [];
+  const { size } = req.query;
+  const limit = size || 10;
+
+  for (let i = 0; i < limit; i++) {
+    products.push({
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(), 10),
+      image: faker.image.urlLoremFlickr(),
+    });
+  }
+
+  res.json(products);
+});
+
+router.get('/filter', (req, res) => {
+  res.send('Soy un filter');
+});
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+
+  res.json({
+    id,
+    name: 'Producto 1',
+    price: 1000,
+  });
+});
+
+module.exports = router;
