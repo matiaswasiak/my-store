50c44b6c20983a14c99ae1b28cc31959bcfaec90
matiaswasiak <matiaswasiak@gmail.com>
2023-07-28 13:58:04 -0300

    GET: recibir parámetros

--- index.js
+++ index.js
@@ -11,9 +11,34 @@ app.get('/nueva-ruta', (req, res) => {
 });
 
 app.get('/productos', (req, res) => {
+  res.json([
+    {
+      id: 1,
+      name: 'Producto 1',
+      price: 1000,
+    },
+    {
+      id: 2,
+      name: 'Producto 2',
+      price: 2000,
+    },
+  ]);
+});
+
+app.get('/products/:id', (req, res) => {
+  const { id } = req.params;
   res.json({
+    id,
     name: 'Producto 1',
     price: 1000,
+  });
+});
+
+app.get('/categories/:categoryId/products/:productId', (req, res) => {
+  const { categoryId, productId } = req.params;
+  res.json({
+    categoryId,
+    productId,
   });
 });
 
