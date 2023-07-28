846cf26ffccd715bcd3db9418bcc92d732367b86
matiaswasiak <matiaswasiak@gmail.com>
2023-07-28 13:47:08 -0300

    Routing con Express.js

--- index.js
+++ index.js
@@ -6,6 +6,17 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.get('/nueva-ruta', (req, res) => {
+  res.send('Nueva ruta');
+});
+
+app.get('/productos', (req, res) => {
+  res.json({
+    name: 'Producto 1',
+    price: 1000,
+  });
+});
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
