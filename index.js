515adb4d1b48ffbb6968ada3ea12ad236166aae4
matiaswasiak <matiaswasiak@gmail.com>
2023-07-29 22:17:00 -0300

    Middleware para HttpErrors

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const routerApi = require('./routes');
 
+const { logErrors, errorHandler } = require('./middlewares/error.handler');
+
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 
 routerApi(app);
+
+app.use(logErrors);
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
