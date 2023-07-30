984cc24f015d75d90469c14fcbf909cd827dd946
matiaswasiak <matiaswasiak@gmail.com>
2023-07-29 22:58:05 -0300

    Probando nuestros endpoints

--- schemas/product.schema.js
+++ schemas/product.schema.js
@@ -3,15 +3,18 @@ const Joi = require('joi');
 const id = Joi.string().uuid();
 const name = Joi.string().alphanum().min(3).max(30);
 const price = Joi.number().integer().min(10);
+const image = Joi.string().uri();
 
 const createProductSchema = Joi.object({
   name: name.required(),
   price: price.required(),
+  image: image.required(),
 });
 
 const updateProductSchema = Joi.object({
   name,
   price,
+  image,
 });
 
 const getProductSchema = Joi.object({
